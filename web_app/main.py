fe8750bc91929013bc337b22153b136ac467d6c2
Bhoomikamd23 <24aiml029@bnmit.in>
2026-04-07 09:31:34 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,8 +1,8 @@
-from flask import flask,render_template
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html',title="Home page")
 if __name__=='__main__':
-  app.run(host='127.0.0.1',port=8080,delay=True)
+  app.run(host='127.0.0.1',port=8080,debug=True)
 
