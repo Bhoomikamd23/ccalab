f725e7a83bbb600df881a3cad1ecbec9cf29ffcb
Bhoomikamd23 <24aiml029@bnmit.in>
2026-04-07 09:29:19 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -3,6 +3,6 @@ app=Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html',title="Home page")
-if__name__=='__main__':
+if __name__=='__main__':
   app.run(host='127.0.0.1',port=8080,delay=True)
 
